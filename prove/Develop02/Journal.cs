997c6d7163bf7344118ec4e4d58ac18babaaea5b
gut23014@byui.edu <matias1805ga@gmail.com>
2024-09-30 19:08:47 -0400

    I added the behaviors and attributtes

--- prove/Develop02/Journal.cs
+++ prove/Develop02/Journal.cs
@@ -1,1 +1,10 @@
+using System;
+public class Journal
+{
+    public List<Entry> _entries = new List<Entry>
 
+    public void DisplayAll(Entry)
+    {
+        Console.WriteLine($"THESE ARE ALL THE ENTRIES");
+    }
+}
