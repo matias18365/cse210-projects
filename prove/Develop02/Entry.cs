b33d99e2975515346e2b5cfddf7f687770154ff7
gut23014@byui.edu <matias1805ga@gmail.com>
2024-10-01 01:27:57 -0400

    adding the feeling tracker

--- prove/Develop02/Entry.cs
+++ prove/Develop02/Entry.cs
@@ -4,11 +4,12 @@ public class Entry
     public string _date;
     public string _promptText;
     public string _entryText;
+    public string _feeling;
 
     public void Display()
     {
         Console.WriteLine($"");
         Console.WriteLine($"Date: {_date} | Prompt: {_promptText}");
-        Console.WriteLine($"{_entryText}");
+        Console.WriteLine($"{_entryText} and I felt {_feeling}");
     }
 }
