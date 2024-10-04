f504803cf7d77cb4b3e35e7448d41cf58f8e4130
gut23014@byui.edu <matias1805ga@gmail.com>
2024-10-03 22:28:35 -0400

    I tested and completed the learning03

--- prepare/Learning03/Fraction.cs
+++ prepare/Learning03/Fraction.cs
@@ -4,8 +4,46 @@ public class Fraction
     private int _top;
     private int _bottom;
 
-    public void Display()
+    public Fraction()
     {
-        Console.WriteLine($"Fraction Class");
+        _top = 1;
+        _bottom = 1;
+    }
+    public Fraction(int wholeNumber)
+    {
+        _top = wholeNumber;
+        _bottom = 1;
+    }
+    public Fraction(int top, int bottom)
+    {
+        _top = top;
+        _bottom = bottom;
+    }
+
+    public int GetTop()
+    {
+        return _top;
+    }
+    public void SetTop(int top)
+    {
+        _top = top;
+    }
+
+    public int GetBottom()
+    {
+        return _bottom;
+    }
+    public void SetBottom(int bottom)
+    {
+        _bottom = bottom;
+    }
+
+    public string GetFractionString()
+    {
+        return $"{_top}/{_bottom}";
+    }
+    public double GetDecimalValue()
+    {
+        return (double)_top / (double)_bottom;
     }
 }
