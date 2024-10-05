53ae406e15a98d1940451fd890e33f9ee4b3ff61
gut23014@byui.edu <matias1805ga@gmail.com>
2024-10-04 21:42:44 -0400

    practicing the clear console

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+        Console.WriteLine("AAA");
+        Console.ReadLine();
+
+// This will clear the console
+        Console.Clear();
+
+// This will show "BBB" in the console where "AAA" used to be
+        Console.WriteLine("BBB");
     }
 }
