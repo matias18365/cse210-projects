00f4b6135171f8fe387e55692fb7d751494f0cfd
gut23014@byui.edu <matias1805ga@gmail.com>
2024-10-12 00:13:00 -0400

    I added the cooment class

---
+++ foundation/Foundation1/Comment.cs
@@ -0,0 +1,19 @@
+using System;
+public class Comment
+{
+    public string _user;
+    public string _commentText;
+    public datetime _commentDate;
+
+    public Comment(string user, string comment, datetime commentDate)
+    {
+        _user = user;
+        _commentText = comment;
+        _commentDate = commentDate;
+    }
+
+    public void DisplayComment()
+    {
+        Console.WriteLine($"{_user} commented on {_commentDate}: "{_commentText}"")
+    }
+}
