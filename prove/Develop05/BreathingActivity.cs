7c640cc449e705818f481265697792b7f3c27f73
gut23014@byui.edu <matias1805ga@gmail.com>
2024-10-19 23:37:25 -0400

    I added the breathing activity attributes and methods

--- prove/Develop05/BreathingActivity.cs
+++ prove/Develop05/BreathingActivity.cs
@@ -1,2 +1,26 @@
 using System;
-class BreathingActivity
+using System.Collections;
+using System.Collections.Generic;
+class BreathingActivity : Activity
+
+{
+    public BreathingActivity() : base("Breathing Activity", "This activity will help you relax by guiding you through slow breathing. Focus on your breathing.")
+    { }
+
+    public void Run()
+    {
+        DisplayStartingMessage();
+        int duration = GetDuration();
+        System.DateTime endTime = System.DateTime.Now.AddSeconds(duration);
+
+        while (System.DateTime.Now < endTime)
+        {
+            Console.WriteLine("Breathe in...");
+            ShowCountDown(4);
+            Console.WriteLine("Breathe out...");
+            ShowCountDown(6);
+        }
+
+        DisplayEndingMessage();
+    }
+}
