fc5cfde9187f2aac22750192859cc6c6d58ada50
gut23014@byui.edu <matias1805ga@gmail.com>
2024-10-19 23:21:29 -0400

    I added the changes for the activity class

--- prove/Develop05/Activity.cs
+++ prove/Develop05/Activity.cs
@@ -1,1 +1,68 @@
-2. Activity.cs
+using System;
+using System.Threading;
+
+public class ReflectingActivity
+{
+    private string _name;
+    private string _description;
+    private int _duration;
+
+    public Activity(string name, string description)
+    {
+        _name = name;
+        _description = description;
+    }
+
+    public void DisplayStartingMessage()
+    {
+        Console.WriteLine($"\nWelcome to the {_name}.");
+        Console.WriteLine($"{_description}");
+        Console.WriteLine($"How long, in second, would you like to do this activity? ");
+        _duration = int,Parse(Console.ReadLine());
+        Console.WriteLine("Get ready...");
+        ShowSpinner(3);
+    }
+
+    public void DisplayEndingMessage()
+    {
+        Console.WriteLine("\nWell done!");
+        Console.WriteLine($"You have completed another {_duration} seconds of the {_name}.")
+    }
+
+    public void ShowSpinner(int seconds)
+    {
+        for (int i = 0; i < seconds; i++)
+        {
+            Console.Write("/");
+            Thread.Sleep(250);
+            Console.Write("\b \b");
+
+            Console.Write("-");
+            Thread.Sleep(250);
+            Console.Write("\b \b");
+
+            Console.Write("\\");
+            Thread.Sleep(250);
+            Console.Write("\b \b");
+
+            Console.Write("|");
+            Thread.Sleep(250);
+            Console.Write("\b \b");
+        }
+    }
+
+    public void ShowCountDown(int seconds)
+    {
+        for (int i = seconds; i > 0; i--)
+        {
+            Console.Write(i);
+            Thread.Sleep(1000);
+            Console.Write("\b \b");
+        }
+    }
+
+    public int GetDuration()
+    {
+        return _duration;
+    }
+}
