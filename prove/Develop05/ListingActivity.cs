d256ffaebba7350348c9ca5f54e697e91fd9b148
gut23014@byui.edu <matias1805ga@gmail.com>
2024-10-20 00:06:04 -0400

    I made changes to the listening activity

---
+++ prove/Develop05/ListingActivity.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+class ListeningActivity : Activity
+{
+    private int _count;
+    private List<string> _prompts = new List<string>
+    {
+        "Who are people that you appreciate?",
+        "What are personal strengths of yours?",
+        "Who are people that you have helped this week?",
+        "Who are some of your personal heroes?"
+    };
+
+    public ListeningActivity() : base ("This activity will help you reflect on times in your life when you have shown strength and resilience. This will help you recognize the power you have and how you can use it in other aspects of your life.")
+    {}
+
+    public void Run()
+    {
+        DisplayStartingMessage();
+        Console.WriteLine(GetRandomPrompt());
+        ShowSpinner(3);
+
+        List<string> items = GetListFromUser();
+        Console.WriteLine($"\nYou listed {items.Count} items.");
+        DisplayEndingMessage();
+    }
+
+    private string GetRandomPrompt()
+    {
+        Random random = new Random();
+        return _prompts[random.Next(_prompts.Count)];
+    }
+
+    private List<string> GetListFromUser();
+    {
+        List<string> items = new List<string>();
+        System.DateTime endTime = System.DateTime.Now.AddSeconds(GetDuration());
+        while (System.DateTime < endTime)
+        {
+            Console.WriteLine("List and item: ");
+            items.Add(Console.ReadLine());
+        }
+        return items;
+    }
+}
